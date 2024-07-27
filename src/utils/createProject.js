61acf421883cb8a9f5f905d8f5ce93b84eb5954d
Aayush Karki <aayush.ecobees@gmail.com>
2024-07-27 12:25:32 +0545

    chore; bump version to 1.0.5

--- src/utils/createProject.js
+++ src/utils/createProject.js
@@ -13,18 +13,18 @@ async function createProject(templateDir, targetDir, projectName, template) {
 
   await fs.copy(templateDir, targetDir);
 
-  // const gitignoreSamplePath = path.join(targetDir, "gitignore.sample");
-  // const gitignorePath = path.join(targetDir, ".gitignore");
+  const gitignoreSamplePath = path.join(targetDir, "gitignore.sample");
+  const gitignorePath = path.join(targetDir, ".gitignore");
 
-  // if (await fs.pathExists(gitignoreSamplePath)) {
-  //   await fs.copy(gitignoreSamplePath, gitignorePath);
-  //   console.log(chalk.green(".gitignore created from sample file"));
+  if (await fs.pathExists(gitignoreSamplePath)) {
+    await fs.copy(gitignoreSamplePath, gitignorePath);
+    console.log(chalk.green(".gitignore created from sample file"));
 
-  //   // Optionally, remove the sample file
-  //   await fs.remove(gitignoreSamplePath);
-  // } else {
-  //   console.warn(chalk.yellow("gitignore.sample not found in template"));
-  // }
+    // Optionally, remove the sample file
+    await fs.remove(gitignoreSamplePath);
+  } else {
+    console.warn(chalk.yellow("gitignore.sample not found in template"));
+  }
 
   // Update package.json
   const packageJsonPath = path.join(targetDir, "package.json");
